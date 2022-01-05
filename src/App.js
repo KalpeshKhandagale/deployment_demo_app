0df1087bca9034acf923d901f40f4ab668c7f474
Kalpesh <kalpesh@Kalpeshs-MacBook-Pro.local>
2022-01-05 13:28:07 +0530

    removed unwanted elements

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   return (
     <div className="App">
       <h1>Breaking the changes!</h1>
-      <p>lorem ipsum is dummy text</p>
-      <div className="App__img">
-        <img src="https://cdn.dribbble.com/users/1707700/screenshots/6592169/dribbble-demo-app_4x.png"/>
-      </div>
     </div>
   );
 }
