52fa18cfb6345d3c87e151c78707b453882844ec
Kalpesh <kalpesh@Kalpeshs-MacBook-Pro.local>
2022-01-05 14:15:39 +0530

    image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'; import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Yes, I can learned to setup the app on netlify!!</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="" />
     </div>
   );
 }
