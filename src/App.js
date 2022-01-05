21fe159e0e7bf5913ff872e1bf785ac58063c681
Kalpesh <kalpesh@Kalpeshs-MacBook-Pro.local>
2022-01-05 12:28:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Deploying is happening...</h1>
+      <p>lorem ipsum is dummy text</p>
+      <div className="App__img">
+        <img src="https://cdn.dribbble.com/users/1707700/screenshots/6592169/dribbble-demo-app_4x.png"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
