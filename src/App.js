f8b63d45e510d199e9170305a71d4734e3b7d941
Kalpesh <kalpesh@Kalpeshs-MacBook-Pro.local>
2022-01-05 13:14:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Deploying is happening...</h1>
+      <h1>Breaking the changes...</h1>
       <p>lorem ipsum is dummy text</p>
       <div className="App__img">
         <img src="https://cdn.dribbble.com/users/1707700/screenshots/6592169/dribbble-demo-app_4x.png"/>
