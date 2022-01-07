5f21b4d8729178fdbb6dcdc2e26e291041fd3f7a
Kalpesh <kalpesh@Kalpeshs-MacBook-Pro.local>
2022-01-07 13:20:20 +0530

    text added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'; import './App.css';
+	import React from 'react';
+	import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Yes, I can learned to setup the app on netlify!!</h1>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="" />
-    </div>
-  );
-}
+	function App() {
+	  return (
+	    <div className="App">
+	      <h1>Yes, I can learned to setup the app on netlify!!</h1>
+	      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="" />
+	      <p>Lorem ipsum is dummy text</p>
+	    </div>
+	  );
+	}
 
-export default App;
+	export default App;
